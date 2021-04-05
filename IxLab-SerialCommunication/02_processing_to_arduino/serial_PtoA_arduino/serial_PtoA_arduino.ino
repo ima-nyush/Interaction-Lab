5a186cfcbcc55feccfa14e9e59ba507face6c0e5
ameliechucky <mar_c_le@yahoo.com>
2021-04-05 19:02:53 +0800

    Update serial_PtoA_arduino.ino

--- IxLab-SerialCommunication/02_processing_to_arduino/serial_PtoA_arduino/serial_PtoA_arduino.ino
+++ IxLab-SerialCommunication/02_processing_to_arduino/serial_PtoA_arduino/serial_PtoA_arduino.ino
@@ -26,6 +26,7 @@ void setup() {
 }
 
 void loop() {
+  getSerialData();
 
   // add your code here using elements in the values array
   
@@ -49,5 +50,57 @@ void loop() {
       */
   //end of example
 
-
+}
+//receive serial data from Processing
+void getSerialData() {
+  if (Serial.available()) {
+    char c = Serial.read();
+    //switch - case checks the value of the variable in the switch function
+    //in this case, the char c, then runs one of the cases that fit the value of the variable
+    //for more information, visit the reference page: https://www.arduino.cc/en/Reference/SwitchCase
+    switch (c) {
+      //if the char c from Processing is a number between 0 and 9
+      case '0'...'9':
+        //save the value of char c to tempValue
+        //but simultaneously rearrange the existing values saved in tempValue
+        //for the digits received through char c to remain coherent
+        //if this does not make sense and would like to know more, send an email to me!
+        tempValue = tempValue * 10 + c - '0';
+        break;
+      //if the char c from Processing is a comma
+      //indicating that the following values of char c is for the next element in the values array
+      case ',':
+        values[valueIndex] = tempValue;
+        //reset tempValue value
+        tempValue = 0;
+        //increment valuesIndex by 1
+        valueIndex++;
+        break;
+      //if the char c from Processing is character 'n'
+      //which signals that it is the end of data
+      case 'n':
+        //save the tempValue
+        //this will b the last element in the values array
+        values[valueIndex] = tempValue;
+        //reset tempValue and valueIndex values
+        //to clear out the values array for the next round of readings from Processing
+        tempValue = 0;
+        valueIndex = 0;
+        break;
+      //if the char c from Processing is character 'e'
+      //it is signalling for the Arduino to send Processing the elements saved in the values array
+      //this case is triggered and processed by the echoSerialData function in the Processing sketch
+      case 'e': // to echo
+        for (int i = 0; i < NUM_OF_VALUES; i++) {
+          Serial.print(values[i]);
+          if (i < NUM_OF_VALUES - 1) {
+            Serial.print(',');
+          }
+          else {
+            Serial.println();
+          }
+        }
+        break;
+    }
+  }
 }

9a394dd1021a826e135a3c8c3ac00172c53119cb
gohai <gottfried.haider@gmail.com>
2024-01-19 05:20:35 +0800

    Add rotary encoder example with interrupts
    @XCF-KHX Have a look at this for inspiration. Believe interrupts are advantageous here, as we don't want to "miss" a step, which could happen when we do other processing inside loop().

---
+++ Tutorials/Rotary Encoder/rotaryencoder_gohai/rotaryencoder_gohai.ino
@@ -0,0 +1,27 @@
+#define PIN_A 2
+#define PIN_B 3
+
+int delta = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_A, INPUT_PULLUP);
+  pinMode(PIN_B, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIN_A), shaft_moved, FALLING);
+}
+
+void loop() {
+  int old = delta;
+  delta = 0;
+  Serial.println(10000+old);
+  delay(100);
+}
+
+void shaft_moved() {
+  int pinb_value = digitalRead(PIN_B);
+  if (pinb_value == HIGH) {
+    delta = delta - 1;
+  } else {
+    delta = delta + 1;
+  }
+}

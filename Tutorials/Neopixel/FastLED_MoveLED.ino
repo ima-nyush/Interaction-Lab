4a8cdd8b50d42a5989916a0cf4854f572acf83a5
Andy-7338 <74288302+Andy-7338@users.noreply.github.com>
2025-10-23 17:32:31 +0800

    Update FastLED_MoveLED.ino

--- Tutorials/Neopixel/FastLED_MoveLED.ino
+++ Tutorials/Neopixel/FastLED_MoveLED.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 
-#define NUM_LEDS  18
+#define NUM_LEDS  60
 #define LED_PIN   3
 
 CRGB leds[NUM_LEDS];
@@ -13,17 +13,16 @@ void setup() {
 void loop() {
 
   for(int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Red;
+    leds[i] = CRGB(255,0,0);
     FastLED.show();
     delay(100);
-    leds[i] = CRGB::Black;
+    leds[i] = CRGB(0,0,0);
   }
 
   for(int i = NUM_LEDS - 1; i >= 0; i--) {
-    leds[i] = CRGB::Red;
+    leds[i] = CRGB(255,0,0);
     FastLED.show();
     delay(100);
-    leds[i] = CRGB::Black;
+    leds[i] = CRGB(0,0,0);
   }
-  
 }

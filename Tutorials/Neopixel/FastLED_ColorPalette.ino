712464c652f2c380fc854591ed2b31b5a62acac9
Andy-7338 <74288302+Andy-7338@users.noreply.github.com>
2025-10-23 17:47:01 +0800

    Update FastLED_ColorPalette.ino

--- Tutorials/Neopixel/FastLED_ColorPalette.ino
+++ Tutorials/Neopixel/FastLED_ColorPalette.ino
@@ -4,34 +4,32 @@
 #define LED_PIN 3
 
 CRGB leds[NUM_LEDS];
-
 int paletteIndex = 0;
 
 CRGBPalette16 myPalette = CRGBPalette16(
-  //
-  CRGB(0, 0, 0),
-  CRGB(50, 50, 100),
-  CRGB(50, 50, 100),
-  CRGB(0, 0, 0),
-  //
-  CRGB(0, 0, 0),
-  CRGB(250, 250, 100),
-  CRGB(250, 250, 100),
-  CRGB(0, 0, 0),
+  CRGB(150, 30, 90),   // magenta
+  CRGB(40, 60, 150),   // indigo blue
+  CRGB(20, 100, 120),  // teal
+  CRGB(160, 90, 30),   // brown
 
-  CRGB(0, 0, 0),
-  CRGB(50, 50, 100),
-  CRGB(50, 50, 100),
-  CRGB(0, 0, 0),
+  CRGB(120, 40, 20),   // red
+  CRGB(170, 120, 60),  // gold
+  CRGB(50, 100, 60),   // green
+  CRGB(100, 60, 130),  // plum
 
-  CRGB(0, 0, 10),
-  CRGB(250, 250, 100),
-  CRGB(50, 50, 100),
-  CRGB(0, 0, 0));
+  CRGB(130, 50, 30),   // orange
+  CRGB(60, 80, 150),   // steel blue
+  CRGB(90, 120, 140),  // cyan-gray
+  CRGB(160, 70, 50),   // copper
+
+  CRGB(110, 50, 100),  // violet
+  CRGB(70, 90, 130),   // blue
+  CRGB(100, 80, 40),   // ochre
+  CRGB(80, 40, 60)     // dark red
+);
 
 void setup() {
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  // FastLED.setBrightness(50);
 }
 
 void loop() {
